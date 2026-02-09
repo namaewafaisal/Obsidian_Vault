34134b54dd0c88d76c0efdf9daf2aee99dcb3257
namaewafaisal <mohamed.faisal.4626@gmail.com>
2026-02-09 11:55:30 +0530

    vault backup: 2026-02-09 11:55:30

--- Leetcode/DSA/Programs/ListMain.java
+++ Leetcode/DSA/Programs/ListMain.java
@@ -4,11 +4,35 @@ class Node{
 
     Node(int data){
         this.data = data;
-        this.next = next;
+        this.next = null;
     }
+
+    public int size(){
+        Node temp = this;
+        int size = 0;
+        while(temp != null){
+            size++;
+            temp.next = t
+        }
+        return size;
+    }
+    public void display(){
+        Node temp = this;
+        while(temp != null){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+
 }
 public class ListMain{
     public static void main(String[] args){
         Node head = new Node(10);
+        Node temp = new Node(20);
+        head.next = temp;
+
+        head.display();
+        System.out.println(head.size());
+        
     }
 }
