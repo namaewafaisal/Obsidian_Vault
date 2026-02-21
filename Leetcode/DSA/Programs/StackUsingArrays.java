f395d710944c0b2cf46cbcd300ffe3f6b6e2987d
namaewafaisal <mohamed.faisal.4626@gmail.com>
2026-02-21 10:37:47 +0530

    vault backup: 2026-02-21 10:37:47

--- Leetcode/DSA/Programs/StackUsingArrays.java
+++ Leetcode/DSA/Programs/StackUsingArrays.java
@@ -1,3 +1,5 @@
+import java.util.EmptyStackException;
+
 public class StackUsingArrays<E> {
     private E arr[];
     private int top;
@@ -5,12 +7,39 @@ public class StackUsingArrays<E> {
 
     StackUsingArrays(int capacity){
         this.top = -1;
-        this.arr = new E[capacity];
+        this.arr = (E[]) new Object[capacity];
         this.capacity = capacity;
+    }
+
+    public boolean isEmpty(){
+        return top <= -1;
+    }
+    public boolean isFull(){
+        return top == capacity-1;
+    }
+    public void push(E data){
+        if(isFull()) throw new StackOverflowError();
+        top++;
+        arr[top] = data;
+    }
+    public E pop(){
+        if(isEmpty()) throw new EmptyStackException();
+        return arr[top--];
+    }
+    public E peek(){
+        if(isEmpty()) throw new EmptyStackException();
+        return arr[top];
+    }
+    public void display(){
+        if(isEmpty()) return;
+        for(int i = top; i>=0; i--){
+            System.out.print(arr[i] + " ");
+        }
     }
 }
 class StackUsingArraysMain{
     public static void main(String[] args) {
         StackUsingArrays<Integer> stack = new StackUsingArrays<>(10);
+        stack.
     }
 }
