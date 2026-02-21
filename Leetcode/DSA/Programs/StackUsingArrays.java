e6f6b2568f896bb7d648e44eed71ad34709f64a9
namaewafaisal <mohamed.faisal.4626@gmail.com>
2026-02-21 09:57:45 +0530

    vault backup: 2026-02-21 09:57:45

---
+++ Leetcode/DSA/Programs/StackUsingArrays.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class StackUsingArrays<E> {
+
+  ArrayList<E> list;
+  int top;
+
+  StackUsingArrays(){
+    top = -1;
+    list = new ArrayList<>();
+  }
+
+  public void push(E data){
+    top
+  }
+}
