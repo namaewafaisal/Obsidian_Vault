c3d9b9430eab63d8c86584c786a1e7d3035dbfed
namaewafaisal <mohamed.faisal.4626@gmail.com>
2026-02-21 10:07:46 +0530

    vault backup: 2026-02-21 10:07:46

--- Leetcode/DSA/Programs/StackUsingArrays.java
+++ Leetcode/DSA/Programs/StackUsingArrays.java
@@ -11,6 +11,26 @@ public class StackUsingArrays<E> {
   }
 
   public void push(E data){
-    top
+    top++;
+    list.add(data);
+  }
+  public E pop(){
+    E value = list.removeLast();
+    top--;
+    return value;
+  }
+  public void display(){
+    for(int i = top; i>=0; i--){
+      System.out.print(list.get)
+    }
+  }
+}
+class StackArrayMain{
+  public static void main(String[] args) {
+    StackUsingArrays<Integer> stack = new StackUsingArrays<>();
+    stack.push(0);
+    stack.push(2);
+    stack.push(3);
+    System.out.print(stack.pop());
   }
 }
