547727f093c4c9dff2936730cb22545dfa2b2e29
namaewafaisal <mohamed.faisal.4626@gmail.com>
2026-02-21 10:17:46 +0530

    vault backup: 2026-02-21 10:17:46

--- Leetcode/DSA/Programs/StackUsingArrays.java
+++ Leetcode/DSA/Programs/StackUsingArrays.java
@@ -1,27 +1,34 @@
 import java.util.ArrayList;
+import java.util.EmptyStackException;
 
 public class StackUsingArrays<E> {
 
-  ArrayList<E> list;
-  int top;
+  private ArrayList<E> list;
 
   StackUsingArrays(){
-    top = -1;
-    list = new ArrayList<>();
+    this.list = new ArrayList<>();
   }
 
   public void push(E data){
-    top++;
     list.add(data);
   }
   public E pop(){
-    E value = list.removeLast();
-    top--;
+    if(list.isEmpty()) throw new EmptyStackException();
+    E value = list.remove(list.size() -1);
     return value;
   }
+
+  public E peek(){
+    if(list.isEmpty()) throw new EmptyStackException();
+    return list.get(list.size()-1);
+  }
+  public boolean isEmpty(){
+    return list.isEmpty();
+  }
+  public 
   public void display(){
-    for(int i = top; i>=0; i--){
-      System.out.print(list.get)
+    for(int i = list.size()-1; i>=0; i--){
+      System.out.print(list.get(i) + " ");
     }
   }
 }
@@ -32,5 +39,10 @@ class StackArrayMain{
     stack.push(2);
     stack.push(3);
     System.out.print(stack.pop());
+    System.out.println();
+    stack.push(5);
+    stack.push(6);
+    stack.display();
+
   }
 }
