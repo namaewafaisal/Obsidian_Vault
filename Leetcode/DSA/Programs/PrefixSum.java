8bb7e5c19b804ad9ad97bd21f4e629f31991679b
namaewafaisal <mohamed.faisal.4626@gmail.com>
2026-03-11 20:26:09 +0530

    vault backup: 2026-03-11 20:26:09

--- Leetcode/DSA/Programs/PrefixSum.java
+++ Leetcode/DSA/Programs/PrefixSum.java
@@ -8,16 +8,22 @@ public class PrefixSum {
         }
     }
     public int rangeSum(int l, int r){
-        if(0 <= l && l < prefix.length )
+        if(l < 0 || prefix.length <= l) throw new IndexOutOfBoundsException();
+        if(r < 0 || prefix.length <= r) throw new IndexOutOfBoundsException();
+        if(l > r) throw new IndexOutOfBoundsException();
         return prefix[r] - (l==0 ? 0 : prefix[l-1]);
     }
     public double rangeAvg(int l, int r){
-        return (prefix[r] - (l==0 ? 0 : prefix[l-1]))/(double) r-l+1;
+        if(l < 0 || prefix.length <= l) throw new IndexOutOfBoundsException();
+        if(r < 0 || prefix.length <= r) throw new IndexOutOfBoundsException();
+        if(l > r) throw new IndexOutOfBoundsException();
+        return (prefix[r] - (l==0 ? 0 : prefix[l-1]))/(double) (r-l+1);
     }
     public static void main(String[] args){
         int[] arr = {1,2,3,4,5};
         PrefixSum ps = new PrefixSum(arr);
-        System.out.println(ps.rangeSum(0,4));
+        System.out.println(ps.rangeSum(3,4));
+        System.out.println(ps.rangeAvg(3,4));
     }
 
 
