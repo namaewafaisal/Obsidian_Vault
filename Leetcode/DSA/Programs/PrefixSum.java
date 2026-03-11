c4d8954ffb536c4a3b5f0ec3e4e3760517f711a4
namaewafaisal <mohamed.faisal.4626@gmail.com>
2026-03-11 20:16:08 +0530

    vault backup: 2026-03-11 20:16:08

--- Leetcode/DSA/Programs/PrefixSum.java
+++ Leetcode/DSA/Programs/PrefixSum.java
@@ -1,5 +1,25 @@
 public class PrefixSum {
     int[] prefix;
 
-    PrefixSum()
+    public PrefixSum(int[] arr){
+        prefix = new int[arr.length];
+        for(int i = 0; i < arr.length; i++){
+            prefix[i] = arr[i] + (i==0 ? 0 : prefix[i-1]);
+        }
+    }
+    public int rangeSum(int l, int r){
+        if(0 <= l && l < prefix.length )
+        return prefix[r] - (l==0 ? 0 : prefix[l-1]);
+    }
+    public double rangeAvg(int l, int r){
+        return (prefix[r] - (l==0 ? 0 : prefix[l-1]))/(double) r-l+1;
+    }
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5};
+        PrefixSum ps = new PrefixSum(arr);
+        System.out.println(ps.rangeSum(0,4));
+    }
+
+
+
 }
