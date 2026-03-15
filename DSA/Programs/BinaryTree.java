af615e6ca917b8d9f2430a24d83fef37422bc428
namaewafaisal <mohamed.faisal.4626@gmail.com>
2026-03-15 12:17:37 +0530

    vault backup: 2026-03-15 12:17:37

--- DSA/Programs/BinaryTree.java
+++ DSA/Programs/BinaryTree.java
@@ -1,5 +1,51 @@
 public class BinaryTree<T> {
-    private class Node<T>{
+    class Node<T>{
+        T data;
+        Node<T> left;
+        Node<T> right;
+        
+        Node(T data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    private Node<T> root;
+
+    public void insert(T data){
+        if(root == null) {
+            root = new Node<T>(data);
+            return;
+        }
+        Queue
+    }
+
+    public void inOrder(Node<T> root){
+        inOrder(root.left);
+        System.out.print(root.data);
+        inOrder(root.right);
+    }
+    public void preOrder(Node<T> root){
+        System.out.print(root.data);
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+    public void postOrder(Node<T> root){
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print();
+    }
+    public void levelOrder(){
+
+    }
+    public int height(){
+
+    }
+    public int size(){
+
+    }
+    public static void main(String[] args) {
         
     }
 }
