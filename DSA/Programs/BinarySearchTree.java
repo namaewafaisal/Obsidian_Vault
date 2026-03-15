2a345aa3f2bc58e63c8467c36fb88f735781b233
namaewafaisal <mohamed.faisal.4626@gmail.com>
2026-03-15 16:39:55 +0530

    vault backup: 2026-03-15 16:39:55

--- DSA/Programs/BinarySearchTree.java
+++ DSA/Programs/BinarySearchTree.java
@@ -13,6 +13,24 @@ public class BinarySearchTree<T extends Comparable<T>>{
     private Node<T> root;
     
     public void insert(T data){
-        insert(root,data);
+        root = insert(root,data);
     }
+    private Node<T> insert(Node<T> root, T data){
+        if(root == null) return new Node<T>(data);
+        if(data.compareTo(root.data) < 0) root.left = insert(root.left, data);
+        else if(data.compareTo(root.data) > 0) root.right = insert(root.right, data);
+        return root;
+    }
+    public boolean search(T data){
+        return search(root,data);
+    }
+    private boolean search(Node<T> root, T data){
+        if(root == null) return false;
+        if(data.compareTo(root.data) < 0) return search(root.left, data);
+        if(data.compareTo(root.data) > 0) return search(root.right, data);
+        return true;
+    }
+    public 
+
+
 }
