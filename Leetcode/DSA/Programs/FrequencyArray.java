a8b6097e56b94774b09d7107e2431bef354d472f
namaewafaisal <mohamed.faisal.4626@gmail.com>
2026-03-15 11:27:34 +0530

    vault backup: 2026-03-15 11:27:34

--- Leetcode/DSA/Programs/FrequencyArray.java
+++ Leetcode/DSA/Programs/FrequencyArray.java
@@ -1,14 +1,43 @@
 public class FrequencyArray {
-    int[] freq;
-    int mostFreq;
+    private int[] freq;
+    private int mostFreq;
     FrequencyArray(int[] arr){
         int maxRange = 0;
         for(int t: arr){
             maxRange = Math.max(maxRange,t);
         }
         freq = new int[maxRange+1];
+        mostFreq = -1;
         for(int t : arr){
-            
+            freq[t]++;
+            if(mostFreq == -1){
+                mostFreq = t;
+            }
+            else if(freq[t] > freq[mostFreq]){
+                mostFreq = t;
+            }
         }
     }
+
+    public int getFrequency(int i){
+        if(i < 0 || i >= freq.length) throw new IndexOutOfBoundsException();
+        return freq[i];
+    }
+    public int getMostFrequent(){
+        return mostFreq;
+    }
+    public boolean contains(int i){
+        if(i < 0 || i >= freq.length) throw new IndexOutOfBoundsException();
+        return freq[i] == 0 ? false : true;
+    }
+    public void display(){
+        for(int i = 0; i<freq.length; i++){
+            System.out.printf("%d : %d\n", i, freq[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        FrequencyArray fq = new FrequencyArray(new int)
+    }
+
 }
