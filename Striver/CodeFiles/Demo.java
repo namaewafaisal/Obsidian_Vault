90e0a72c22e1bdb5fa2c0caa5aa030566aef29e0
namaewafaisal <mohamed.faisal.4626@gmail.com>
2026-08-16 21:48:39 +0530

    vault backup: 2026-08-16 21:48:39

--- Striver/CodeFiles/Demo.java
+++ Striver/CodeFiles/Demo.java
@@ -4,6 +4,6 @@ public class Demo {
         
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.println(n*n);
+        System.out.println("Hello " + n);
     }
 }
